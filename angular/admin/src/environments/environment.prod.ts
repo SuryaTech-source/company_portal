a18f55937e9d1255c67174489d345c36ecdb7bdd
amalabraham93 <amalabraham93@gmail.com>
2025-10-14 12:34:38 +0530

    Update production environment API URL to localhost for local testing

--- angular/admin/src/environments/environment.prod.ts
+++ angular/admin/src/environments/environment.prod.ts
@@ -4,6 +4,8 @@
   // apiUrl: "https://ecommerce-dev.quickiz.com/",
   // apiUrl: "https://clickkart.zoproduct.com/",
   // apiUrl: "https://pillais.quickiz.com/",
+    apiUrl: "http://localhost:2023/",
+
   // apiUrl: "https://grocery.quickiz.com/",
   mapKey: 'AIzaSyBxvZ4uekYJgs7atKyK3x0UOhZzcYw19Rg'
 };
