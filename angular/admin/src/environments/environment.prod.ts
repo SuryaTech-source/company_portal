abc08eec694f8fd823fdd4cb76eeb816fb33990c
amalabraham93 <amalabraham93@gmail.com>
2025-10-17 16:32:34 +0530

    Update API URLs in environment files for production and development configurations

--- angular/admin/src/environments/environment.prod.ts
+++ angular/admin/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@
   // apiUrl: 'https://ecommerce-testing.quickiz.com/',
   // apiUrl: "https://ecommerce-dev.quickiz.com/",
   // apiUrl: "https://clickkart.zoproduct.com/",
-  // apiUrl: "https://pillais.quickiz.com/",
-    apiUrl: "http://localhost:2023/",
+  apiUrl: "https://companyportal-production.up.railway.app/",
+    // apiUrl: "http://localhost:2023/",
 
   // apiUrl: "https://grocery.quickiz.com/",
   mapKey: 'AIzaSyBxvZ4uekYJgs7atKyK3x0UOhZzcYw19Rg'
