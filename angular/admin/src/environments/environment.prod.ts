758e5a9c5eea913c72c1a5073b92f863f6f29594
amalabraham93 <amalabraham93@gmail.com>
2025-12-16 14:10:28 +0530

    fix(environment): update apiUrl to use 'https://samivlandmarks.in/tms/api/' and comment out old production URL

--- angular/admin/src/environments/environment.prod.ts
+++ angular/admin/src/environments/environment.prod.ts
@@ -3,7 +3,8 @@
   // apiUrl: 'https://ecommerce-testing.quickiz.com/',
   // apiUrl: "https://ecommerce-dev.quickiz.com/",
   // apiUrl: "https://clickkart.zoproduct.com/",
-  apiUrl: "https://companyportal-production-7c21.up.railway.app/",
+  // apiUrl: "https://companyportal-production-7c21.up.railway.app/",
+   apiUrl: 'https://samivlandmarks.in/tms/api/',
     // apiUrl: "http://localhost:2023/",
 
   // apiUrl: "https://grocery.quickiz.com/",
