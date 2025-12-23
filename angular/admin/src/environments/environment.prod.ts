7f60fce70267a753af9d4fb2b9e78c32d1f6553e
root <root@66-116-204-49.webhostbox.net>
2025-12-23 14:30:01 +0000

    Update prod API URL for server

--- angular/admin/src/environments/environment.prod.ts
+++ angular/admin/src/environments/environment.prod.ts
@@ -3,9 +3,13 @@
   // apiUrl: 'https://ecommerce-testing.quickiz.com/',
   // apiUrl: "https://ecommerce-dev.quickiz.com/",
   // apiUrl: "https://clickkart.zoproduct.com/",
-  // apiUrl: "https://companyportal-production-7c21.up.railway.app/",
-   apiUrl: 'https://samivlandmarks.in/tms/api/',
-    // apiUrl: "http://localhost:2023/",
+
+
+
+// apiUrl: "https://companyportal-production-7c21.up.railway.app/",
+  apiUrl: 'https://samivlandmarks.in/tms/api/',
+ // apiUrl: "http://localhost:2023/",
+
 
   // apiUrl: "https://grocery.quickiz.com/",
   mapKey: 'AIzaSyBxvZ4uekYJgs7atKyK3x0UOhZzcYw19Rg'
